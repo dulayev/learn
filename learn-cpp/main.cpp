9386ec5c74395264e1ccc9af5abbdbce66b83287
dulayev <33511608+dulayev@users.noreply.github.com>
2018-02-13 14:42:22 +0300

    check need virtual keyword when overriding

--- learn-cpp/main.cpp
+++ learn-cpp/main.cpp
@@ -1,10 +1,38 @@
 #include "ChronoTest.h"
 
+//TODO: move this to separate file
+#include <iostream>
 using namespace std;
 
-int main(int argc, char** argv) {
+class Grand {
+public:
+	virtual void Report()  { cout << "Grand"; }
+};
+
+class Parent : public Grand {
+public:
+	 // no matter has virtual or not
+	void Report() override { cout << "Parent"; }	
+};
+
+class Child : public Parent {
+public:
+	virtual void Report() override { cout << "Child"; }
+};
+
+int main() {
+
+	Child child;
+	
+	Grand* grand = &child;
+	
+	grand->Report();
+	
+	Parent* parent = &child;
+	
+	parent->Report();
 
     ChronoTest();
-    
+
     return 0;
 }
