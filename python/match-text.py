aaeafd1f0f06af695c5aad9e29ee796e6764252f
Aleksey Dulaev <aleksey.dulaev@tra.ai>
2020-12-06 16:35:47 +0300

    Prototyping fast longest matching substring

--- python/match-text.py
+++ python/match-text.py
@@ -1,30 +1,37 @@
 import collections
 
-MatchState = collections.namedtuple('MatchState', ['match_count', 'transitions'])
+# MatchState = collections.namedtuple('MatchState', ['match_count', 'transitions'])
 
-def create_match_machine(text):
-    state_machine = []
-    state = MatchState(0, {})
-    state_machine.append(state)
+def create_text_stats(line):
+    char_poses = {}
+    for i, c in enumerate(line):
+        char_poses.setdefault(c, []).append(i)
+    return char_poses
 
-    def func_recur(state, start_pos):
-                
-
-    func_recur(state, 0)
-
-    for i in range(len(text)):
-        
-    return state_machine
-
-def exec_match_machine(machine, text):
-    state = machine[0]
-
+def find_longest_match(line_stats, text):
+    max_len = 0
+    prev = {} # pos->len ending at this pos
     for c in text:
-        if c in state.transitions:
-            state = machine[state.transitions[c]]
-        else:
-            return state.match_count
+        curr = {}
+        if c in line_stats:
+            for pos in line_stats[c]:
+                if pos - 1 in prev:
+                    curr[pos] = prev[pos - 1] + 1
+                else:
+                    curr[pos] = 1
+                if max_len < curr[pos]:
+                    max_len = curr[pos]
+        prev = curr
+    return max_len
 
-machine = create_match_machine("fox")
-count = exec_match_machine(machine, "fox on street")
-assert(count == 3)
+test_tuples = [
+    ("fox", "fox on street", 3),
+    ("fox", "holy fox on street", 3),
+    ("fox", "holy fox", 3),
+    ("eef", "---ef----", 2),
+    ("eef", "---eef----", 3)
+]
+
+for t in test_tuples:
+    assert(find_longest_match(create_text_stats(t[0]), t[1]) == t[2])
+
